5a16cd342ec81695b68cd41fa9b57bd1e7eabe35
Gloria <gloria.martinez.magallon@gmail.com>
2017-10-12 10:03:10 +0200

    idioma es, variables de idioma, quitar registro

---
+++ resources/lang/es/app.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'login'         => [
+        'title'             => 'Inicio de sesión',
+        'email'             => 'Correo electrónico',
+        'password'          => 'Contraseña',
+        'rememberme'        => 'Recorderme',
+        'btn'               => 'Acceder',
+        'btnforgotpassword' => '¿Olvidó su contraseña?',
+    ],
+    'resetpassword' => [
+        'title'           => 'Restablecer contraseña',
+        'email'           => 'Correo electrónico',
+        'btnsendlink'     => 'Enviar enlace al correo electrónico',
+        'password'        => 'Contraseña',
+        'confirmpassword' => 'Confirmar contraseña',
+
+    ],
+];
